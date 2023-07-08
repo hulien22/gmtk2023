644ee30d290bf2f4364bf27198ce0787cbbbe6d7
Eimh <41556634+Eimh@users.noreply.github.com>
2023-07-08 19:53:10 -0400

    joe voice

--- scenes/main.gd
+++ scenes/main.gd
@@ -132,6 +132,7 @@ func check_loop():
 		$Grid2.disable_all_clickable_tiles()
 		# wait a bit in between drops
 		await get_tree().create_timer(0.4).timeout
+	AudioAutoload.play_voice(cascades)
 
 func select_finger_swap():
 	# TODO do we need to be smarter about this to stop player from just corner camping?
